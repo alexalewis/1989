b47d6179f7879b3233fbbc277505f8e9a537cc90
Alexa Lewis <alexalewisdevelops@gmail.com>
2020-03-25 15:06:27 -0400

    made array of movies

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # 1989
+
+This is the API end-point that supplied the data file for this project:
+
+https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=YOUR_KEY_HERE
+
+You will need to sign up and make an account to get an API key.
+
+An example of the movie object is
+
+{
+"vote_count": 4300,
+"id": 89,
+"video": false,
+"vote_average": 7.7,
+"title": "Indiana Jones and the Last Crusade",
+"popularity": 18.43,
+"poster_path": "/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg",
+"original_language": "en",
+"original_title": "Indiana Jones and the Last Crusade",
+"genre_ids": [12, 28],
+"backdrop_path": "/vfvVuu1JdnEGcyZUj7VHrhhbeMj.jpg",
+"adult": false,
+"overview":
+"When Dr. Henry Jones Sr. suddenly goes missing while pursuing the Holy Grail, eminent archaeologist Indiana must team up with Marcus Brody, Sallah and Elsa Schneider to follow in his father's footsteps and stop the Nazis from recovering the power of eternal life.",
+"release_date": "1989-05-24"
+}
+You'll notice that the poster images are just the file names. You can get the full URL to a poster by concatenating the path with the following base URL:
+
+https://image.tmdb.org/t/p/w185_and_h278_bestv2
+In this case:
+
+https://image.tmdb.org/t/p/w185_and_h278_bestv2/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg
+Objectives
+
+Work with API data
+Understand and use React components
+Understand and use "props" in React
+Use Array.prototype.map to render a collection of components in React
+
+Use the API to fetch the data and render it on the page, rather than the supplied JSON file.
+You should have at least two components; App and Movie.
+Render at least the title, poster image, and plot overview. Feel free to include other pieces of the data.
+Try to capture an 1980's or early 90's aesthetic when you style your page. Have fun with it!
--- src/App.js
+++ src/App.js
@@ -1,8 +1,72 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React from 'react'
+
+const Movie = () => {
+  const posterURL = `https://image.tmdb.org/t/p/w185_and_h278_bestv2/"${props.poster_path}`
+  return (
+    <li>
+      <p>{props.title}</p>
+      <div class="movie-image-and-caption">
+        <img src={posterURL}></img>
+        <p>{props.overview}</p>
+      </div>
+    </li>
+  )
+}
 
 const App = () => {
-  return <HelloWorld />
+  const movies = [
+    {
+      vote_count: 4300,
+      id: 89,
+      video: false,
+      vote_average: 7.7,
+      title: 'Indiana Jones and the Last Crusade',
+      popularity: 18.43,
+      poster_path: '/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg',
+      original_language: 'en',
+      original_title: 'Indiana Jones and the Last Crusade',
+      genre_ids: [12, 28],
+      backdrop_path: '/vfvVuu1JdnEGcyZUj7VHrhhbeMj.jpg',
+      adult: false,
+      overview:
+        "When Dr. Henry Jones Sr. suddenly goes missing while pursuing the Holy Grail, eminent archaeologist Indiana must team up with Marcus Brody, Sallah and Elsa Schneider to follow in his father's footsteps and stop the Nazis from recovering the power of eternal life.",
+      release_date: '1989-05-24',
+    },
+  ]
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=a6d1cf27a5747141f1db754bed4bc307`
+    )
+      .then((response) => response.json())
+      .then((apiData) => {
+        // console.log(apiData)
+        const movies = apiData.results
+        console.log(movies)
+      })
+  }, [])
+
+  return (
+    <main>
+      <header>
+        <h1>Party Like It is 1989</h1>
+        <p>Made for SDG</p>
+      </header>
+      {/* some list of movies */}
+      <ul class="movies">
+        {movies.map((movie) => {
+          return (
+            <Movie
+              title={movie.title}
+              poster_path={movie.poster_path}
+              overview={movie.overview}
+            />
+          )
+        })}
+      </ul>
+      <footer>Made to shower the power of REACT</footer>
+    </main>
+  )
 }
 
 export default App
